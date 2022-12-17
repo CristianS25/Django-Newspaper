490f75fd0d92f17bbe28f4dea2b17753b60108ce
Cristian <cristiandqs1234@gmail.com>
2022-12-17 11:38:13 -0500

    creación del repositorio

---
+++ app_newspaper/app_newspaper/urls.py
@@ -0,0 +1,33 @@
+"""app_newspaper URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django. views.generic.base import TemplateView
+#importamos el capturador del error
+from django.conf.urls import handler403
+#importamos la vista del error
+from pages.views import ErrorView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('articles.urls')),
+    path('', include('pages.urls')),
+]
+
+#igualamos el error con la vista
+handler403 = ErrorView.as_view()
---
+++ app_newspaper/pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class ErrorView(TemplateView):
+    template_name = 'error.html'
+    
